20f71d05d2ee158a90421e75d3767803244a2bfb
enenChen <35317791+enenChen@users.noreply.github.com>
2021-01-06 14:42:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import sys
 
 from maotai.jd_spider_requests import JdSeckill
 
+def get_choice_from_arg():
+    """
+        添加带参数快速启动脚本的特性，可以搭配Windows任务计划程序，早上定时触发抢茅台。
+    """
+    import argparse
+    parser = argparse.ArgumentParser(description='Automatic seckill maotai on jd.com, good luck!')
+    parser.add_argument("-c", "--choice", type=str, default=None, help="id of the choice_function, '1' for resrve and '2' for seckill.")
+    args = parser.parse_args()
+    return args.choice
+
 if __name__ == '__main__':
     a = """
-
        oooo oooooooooo.            .oooooo..o                     oooo         o8o  oooo  oooo  
        `888 `888'   `Y8b          d8P'    `Y8                     `888         `"'  `888  `888  
         888  888      888         Y88bo.       .ooooo.   .ooooo.   888  oooo  oooo   888   888  
@@ -21,7 +30,8 @@ if __name__ == '__main__':
     print(a)
 
     jd_seckill = JdSeckill()
-    choice_function = input('请选择:')
+    choice_from_arg = get_choice_from_arg()
+    choice_function = choice_from_arg if choice_from_arg else input('请选择:')
     if choice_function == '1':
         jd_seckill.reserve()
     elif choice_function == '2':
@@ -29,4 +39,3 @@ if __name__ == '__main__':
     else:
         print('没有此功能')
         sys.exit(1)
-
